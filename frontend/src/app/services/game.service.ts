3bfa07530b67ff474cf182b7acddb4f402dc8c01
norby97 <simonorbi97@gmail.com>
2018-06-20 04:45:39 +0300

    elso

--- frontend/src/app/services/game.service.ts
+++ frontend/src/app/services/game.service.ts
@@ -16,20 +16,20 @@ export class GameService {
     kerdesLekeres(){
       let headers = new Headers();
       headers.append('Content-Type','application/json');
-      return this.http.get('http://localhost:3000/games/game', {headers:headers}).map(res => res.json());
+      return this.http.get('games/game', {headers:headers}).map(res => res.json());
     }
 
 //kerdes hozzaadas keres elkuldese
     kerdesHozzaadas(kerdes){
     let headers = new Headers();
     headers.append('Content-Type','application/json');
-    return this.http.post('http://localhost:3000/games/addquestion', kerdes, {headers:headers}).map(res => res.json());
+    return this.http.post('games/addquestion', kerdes, {headers:headers}).map(res => res.json());
   }
 
 //elkuldi a szervernek a pontszamot hogy mentse le
     pontszamMentes(felhasznalo){
     let headers = new Headers();
     headers.append('Content-Type', 'application/json');
-    return this.http.post('http://localhost:3000/users/savegame', felhasznalo, {headers:headers}).map(res => res.json());
+    return this.http.post('users/savegame', felhasznalo, {headers:headers}).map(res => res.json());
   }
 }
